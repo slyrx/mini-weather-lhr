6e5980a5cdbd464c43f28f410c50613f493768e6
slyrx <slyrx2006@gmail.com>
2016-09-29 00:10:25 +0800

    first layout ok

---
+++ app/src/main/java/cn/edu/pku/slyrx/miniweatherlhr/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.slyrx.miniweatherlhr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by slyrx on 16/9/26.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
