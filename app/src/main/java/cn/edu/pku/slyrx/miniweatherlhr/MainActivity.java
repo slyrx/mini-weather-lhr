e0652c9122f2b03eeed42d68b80edc8718fa25a1
slyrx <slyrx2006@gmail.com>
2016-12-20 15:41:20 +0800

    add show latitude and Longitude

--- app/src/main/java/cn/edu/pku/slyrx/miniweatherlhr/MainActivity.java
+++ app/src/main/java/cn/edu/pku/slyrx/miniweatherlhr/MainActivity.java
@@ -1,11 +1,21 @@
 package cn.edu.pku.slyrx.miniweatherlhr;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,6 +33,7 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Iterator;
 
 import cn.edu.pku.slyrx.bean.TodayWeather;
 import cn.edu.pku.slyrx.util.NetUtil;
@@ -35,7 +46,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private static final int UPDATE_TODAY_WEATHER = 1;
     private static final String TAG = "MyMainActivity";
 
-    private ImageView mUpdateBtn;
+    private ImageView mUpdateBtn, mLocationBtn;
     private ImageView mCitySelect;
 
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv, temperatureTv, climateTv, windTv, city_name_Tv, temperTodayTv;
@@ -45,7 +56,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private updateService mUpdateService = null;
 
-    void updateTodayWeather(TodayWeather todayWeather){
+    private LocationManager mLocationManager = null;
+
+    void updateTodayWeather(TodayWeather todayWeather) {
         city_name_Tv.setText(todayWeather.getCity() + "天气");
         cityTv.setText(todayWeather.getCity());
         timeTv.setText(todayWeather.getUpdatetime() + "发布");
@@ -60,32 +73,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Toast.makeText(MainActivity.this, "更新成功!", Toast.LENGTH_SHORT).show();
     }
 
-    private Handler mHandler = new Handler(){
-      public void handleMessage(android.os.Message msg){
-          switch (msg.what){
-              case UPDATE_TODAY_WEATHER:
-                  updateTodayWeather((TodayWeather)msg.obj);
-                  break;
-              default:
-                  break;
-          }
-      }
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case UPDATE_TODAY_WEATHER:
+                    updateTodayWeather((TodayWeather) msg.obj);
+                    break;
+                default:
+                    break;
+            }
+        }
     };
 
-    void initView(){
-        city_name_Tv = (TextView)findViewById(R.id.title_city_name);
-        cityTv = (TextView)findViewById(R.id.city);
-        timeTv = (TextView)findViewById(R.id.time);
-        humidityTv = (TextView)findViewById(R.id.humidity);
-        weekTv = (TextView)findViewById(R.id.week_today);
-        pmDataTv = (TextView)findViewById(R.id.pm_data);
-        pmQualityTv = (TextView)findViewById(R.id.pm2_5_quality);
-        pmImg = (ImageView)findViewById(R.id.pm2_5_img);
-        temperatureTv = (TextView)findViewById(R.id.temperature);
-        climateTv = (TextView)findViewById(R.id.climate);
-        windTv = (TextView)findViewById(R.id.wind);
-        weatherImg = (ImageView)findViewById(R.id.weather_img);
-        temperTodayTv = (TextView)findViewById(R.id.temperature_current);
+    void initView() {
+        city_name_Tv = (TextView) findViewById(R.id.title_city_name);
+        cityTv = (TextView) findViewById(R.id.city);
+        timeTv = (TextView) findViewById(R.id.time);
+        humidityTv = (TextView) findViewById(R.id.humidity);
+        weekTv = (TextView) findViewById(R.id.week_today);
+        pmDataTv = (TextView) findViewById(R.id.pm_data);
+        pmQualityTv = (TextView) findViewById(R.id.pm2_5_quality);
+        pmImg = (ImageView) findViewById(R.id.pm2_5_img);
+        temperatureTv = (TextView) findViewById(R.id.temperature);
+        climateTv = (TextView) findViewById(R.id.climate);
+        windTv = (TextView) findViewById(R.id.wind);
+        weatherImg = (ImageView) findViewById(R.id.weather_img);
+        temperTodayTv = (TextView) findViewById(R.id.temperature_current);
 
         city_name_Tv.setText("N/A");
         cityTv.setText("N/A");
@@ -110,24 +123,28 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
         mUpdateBtn.setOnClickListener(this);
+        mLocationBtn = (ImageView) findViewById(R.id.title_location);
+        mLocationBtn.setOnClickListener(this);
 
         //检查网络是否可用
-        if(NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE){
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE) {
             Log.d("myMiniWeather", "网络ok");
             Toast.makeText(MainActivity.this, "网络ok！", Toast.LENGTH_LONG).show();
             SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
             String cityCode = sharedPreferences.getString("main_city_code", "101010100");
             queryWeatherCode(cityCode);
 
-        }else {
+        } else {
             Log.d("myMiniWeather", "网络down");
             Toast.makeText(MainActivity.this, "网络down！", Toast.LENGTH_LONG).show();
         }
 
-        mCitySelect = (ImageView)findViewById(R.id.title_city_manager);
+        mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
         mCitySelect.setOnClickListener(this);
 
         initView();
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         //开启服务，用以更新天气数据
 //        mUpdateService = new updateService();
@@ -137,9 +154,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     @Override
-    public void onClick(View view){
+    public void onClick(View view) {
 
-        if(view.getId() == R.id.title_city_manager){
+        if (view.getId() == R.id.title_city_manager) {
 
 
             Intent i = new Intent(this, SelectCity.class);
@@ -147,7 +164,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         }
 
-        if(view.getId() == R.id.title_update_btn){
+        switch (view.getId()){
+            case R.id.title_update_btn:
+                updateBtnRun();
+                break;
+            case R.id.title_location:
+                Location curLoc = getLocation();
+                Toast.makeText(MainActivity.this, "经度: "+ curLoc.getLatitude() + "纬度: " + curLoc.getLongitude(), Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+
+        if (view.getId() == R.id.title_update_btn) {
 //            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
 //            String cityCode = sharedPreferences.getString("main_city_code", "101010100");
 //            if(null != mSelectCityNum) {
@@ -168,25 +197,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     }
 
-    public void updateBtnRun(){
+    public void updateBtnRun() {
         SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
         String cityCode = sharedPreferences.getString("main_city_code", "101010100");
-        if(null != mSelectCityNum) {
+        if (null != mSelectCityNum) {
             cityCode = mSelectCityNum;
         }
         Log.d("myMiniWeather", cityCode);
 
         //检查网络是否可用
-        if(NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE){
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE) {
             Log.d("myMiniWeather", "网络ok");
             queryWeatherCode(cityCode);
-        }else {
+        } else {
             Log.d("myMiniWeather", "网络down");
             Toast.makeText(MainActivity.this, "网络down！", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void queryWeatherCode(String cityCode){
+    private void queryWeatherCode(String cityCode) {
         final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
         Log.d("myMiniWeather", address);
 
@@ -196,9 +225,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 HttpURLConnection con = null;
                 TodayWeather todayWeather = null;
 
-                try{
+                try {
                     URL url = new URL(address);
-                    con = (HttpURLConnection)url.openConnection();
+                    con = (HttpURLConnection) url.openConnection();
                     con.setRequestMethod("GET");
                     con.setConnectTimeout(8000);
                     con.setReadTimeout(8000);
@@ -207,15 +236,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
                     StringBuilder response = new StringBuilder();
                     String str;
-                    while ((str=reader.readLine()) != null){
+                    while ((str = reader.readLine()) != null) {
                         response.append(str);
                         Log.d("myMiniWeather", str);
                     }
 
-                    String responseStr=response.toString();
+                    String responseStr = response.toString();
                     Log.d("myMiniWeather", responseStr);
                     todayWeather = parseXML(responseStr);
-                    if(todayWeather != null){
+                    if (todayWeather != null) {
                         Log.d("myMiniWeather", todayWeather.toString());
 
                         Message msg = new Message();
@@ -224,10 +253,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
                         mHandler.sendMessage(msg);
                     }
 
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
-                }finally {
-                    if(con != null){
+                } finally {
+                    if (con != null) {
                         con.disconnect();
                     }
                 }
@@ -235,7 +264,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }).start();
     }
 
-    private TodayWeather parseXML(String xmldata){
+    private TodayWeather parseXML(String xmldata) {
         TodayWeather todayWeather = null;
 
         int fengxiangCount = 0;
@@ -245,7 +274,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         int lowCount = 0;
         int typeCount = 0;
 
-        try{
+        try {
             XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
             XmlPullParser xmlPullParser = fac.newPullParser();
             xmlPullParser.setInput(new StringReader(xmldata));
@@ -253,16 +282,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
             int eventType = xmlPullParser.getEventType();
             Log.d("myMiniWeather", "parseXML");
 
-            while (eventType != XmlPullParser.END_DOCUMENT){
-                switch (eventType){
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
                     case XmlPullParser.START_DOCUMENT:
                         break;
                     case XmlPullParser.START_TAG:
-                        if(xmlPullParser.getName().equals("resp")){
+                        if (xmlPullParser.getName().equals("resp")) {
                             todayWeather = new TodayWeather();
                         }
 
-                        if(todayWeather != null) {
+                        if (todayWeather != null) {
                             if (xmlPullParser.getName().equals("city")) {
                                 eventType = xmlPullParser.next();
                                 todayWeather.setCity(xmlPullParser.getText());
@@ -315,9 +344,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 eventType = xmlPullParser.next();
             }
 
-        }catch (XmlPullParserException e){
+        } catch (XmlPullParserException e) {
             e.printStackTrace();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -331,13 +360,129 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Bundle bundle = data.getBundleExtra("GetSelectCityNumber");
         String str = bundle.getString("City");
 
-        if(110 == requestCode && 101 == resultCode){
+        if (110 == requestCode && 101 == resultCode) {
             mSelectCityNum = str;
 
             //同时更新画面
-            if(null != mSelectCityNum) {
+            if (null != mSelectCityNum) {
                 queryWeatherCode(mSelectCityNum);
             }
         }
     }
+
+    private Location getLocation() {
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+        Location gpsLocation = null;
+        Location netLocation = null;
+
+        //状态监听
+        GpsStatus.Listener gpsListener = new GpsStatus.Listener() {
+            @Override
+            public void onGpsStatusChanged(int event) {
+                switch (event) {
+                    case GpsStatus.GPS_EVENT_FIRST_FIX:
+                        break;
+                    case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                        GpsStatus gpsStatus = mLocationManager.getGpsStatus(null);
+                        int maxSatellites = gpsStatus.getMaxSatellites();
+                        Iterator<GpsSatellite> iters = gpsStatus.getSatellites().iterator();
+                        int count = 0;
+                        while (iters.hasNext() && count <= maxSatellites) {
+                            GpsSatellite s = iters.next();
+                            count++;
+                        }
+                        break;
+                    case GpsStatus.GPS_EVENT_STARTED:
+                        break;
+                    case GpsStatus.GPS_EVENT_STOPPED:
+                        break;
+                }
+            }
+        };
+
+
+        mLocationManager.addGpsStatusListener(gpsListener);
+
+        if(netWorksOpen()){
+            mLocationManager.requestLocationUpdates("network", 20000, 5, locationListener);
+            netLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+        if(gpsIsOpen()){
+            mLocationManager.requestLocationUpdates("gps", 20000, 5, locationListener);
+            gpsLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+
+        if (gpsLocation == null && netLocation == null) {
+            return null;
+        }
+
+        if (gpsLocation != null && netLocation != null) {
+            if (gpsLocation.getTime() < netLocation.getTime()) {
+                gpsLocation = null;
+                return netLocation;
+            } else {
+                netLocation = null;
+                return gpsLocation;
+            }
+        }
+
+        if (gpsLocation == null) {
+            return netLocation;
+        } else {
+            return gpsLocation;
+        }
+    }
+
+    private boolean netWorksOpen(){
+        boolean netlsOpen = true;
+
+        if(!mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            netlsOpen = false;
+        }
+
+        return netlsOpen;
+    }
+
+    private boolean gpsIsOpen() {
+        boolean isOpen = true;
+        if (!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {//没有开启GPS
+            isOpen = false;
+        }
+        return isOpen;
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(location != null){
+                //同步数据
+            }else {
+                //获取不到数据
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
---
+++ app/src/main/java/cn/edu/pku/slyrx/util/MyAsyncExtue.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.slyrx.util;
+
+import android.location.Location;
+import android.os.AsyncTask;
+
+/**
+ * Created by slyrx on 2016/12/18.
+ */
+public class MyAsyncExtue extends AsyncTask<Location, Void, String> {
+
+    @Override
+    protected String doInBackground(Location... params) {
+
+        return null;
+    }
+}
